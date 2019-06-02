d19b9b977ab51513f84703e3bb09ffe40b70a237
Benjamin Bollen <ben@ost.com>
2019-06-02 20:27:21 +0200

    credit: more iterations

--- contracts/rules/Credit.sol
+++ contracts/rules/Credit.sol
@@ -14,32 +14,40 @@ pragma solidity ^0.5.0;
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 import "./ExtensionRuleI.sol";
 import "./FirewalledRule.sol";
 import "../token/EIP20TokenInterface.sol";
 
 contract Credit is ExtensionRuleI, FirewalledRule {
 
-    // token
-    EIP20TokenInterface public token;
-
-    // allocated budget to credits; keep these tokens locked in the contract
-    uint256 public allocatedBudget;
+    using SafeMath for uint256;
 
     // Sentinel to mark end of generations linked list.
+    // set to max unix epoch time for 256-bits
     uint256 public constant SENTINEL_GENERATIONS = uint256(0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff);
+    // uint256 public constant SENTINEL_GENERATIONS = uint256(0x0000000000000000000000000000000000000000000000000000000000000001);
 
     // Bound the number of maximum (active) generations to keep performance
     uint8 public constant MAX_GENERATIONS = uint8(15);
+    
+    // token
+    EIP20TokenInterface public token;
 
-    // Issuer of credits // use onlyOrganization
-    // address public issuer;
+    // total allocated budget to credits; keep these tokens locked in the contract
+    uint256 public totalAllocatedBudget;
+
+    // track allocated budget per generation for efficient freeing up budget
+    // on expiring generations
+    mapping(uint256 => uint256)
 
     // mapping of generation to credit balances
     mapping(uint256 => mapping(address => uint256)) public credits;
 
     // linked list of generations expressed by their expiration time in
-    // seconds since unix epoch; ordered in increasing expiration time.
+    // seconds since unix epoch; ordered in later expiration time.
+    // ie. earliest / "oldest" is first in the linked-list
+    // mapping points to "next generation"
     mapping(uint256 => uint256) public generations;
 
     // whitelist of destination addresses [under consideration; OPTIONAL]
@@ -53,7 +61,7 @@ contract Credit is ExtensionRuleI, FirewalledRule {
         _;
     }
 
-    // function constructor
+    // constructor
 
     function extend
     (
@@ -69,7 +77,11 @@ contract Credit is ExtensionRuleI, FirewalledRule {
         address[] memory transfersAmount_
     )
     {
-
+        // check to addresses [optional, on whitelisting]
+        // sum amounts to spending_total
+        // iterate over the generations, earliest expiration first
+        // consume credits and sum total up to spending_total
+        // transfer from credits to _from,
     }
 
     // insert new generation only in correct ordered position in linked list.
@@ -78,6 +90,7 @@ contract Credit is ExtensionRuleI, FirewalledRule {
         onlyIssuer
         returns (bool)
     {
+
         // cannot be SENTINEL; cannot be added already
         // insert generation only at correct ordered position
         // respect max length
@@ -91,11 +104,14 @@ contract Credit is ExtensionRuleI, FirewalledRule {
         onlyIssuer
         // returns (bool) // what should it return?
     {
-        uint256 balance = token.balanceOf(this); // get the token balance
+        uint256 balance = token.balanceOf(address(this)); // get the token balance
         assert(balance >= allocatedBudget);
         require(balance - allocatedBudget >= _amount,
             "Insufficient balance to allocate credit.");
-        
+        require(isValidGeneration(_generation),
+            "Generation must be valid.");
+        credits[_generation][_holder] = credits[_generation][_holder].add(_amount);
+        allocatedBudget = allocatedBudget.
     }
 
 
@@ -119,6 +135,19 @@ contract Credit is ExtensionRuleI, FirewalledRule {
         onlyOrganization
     {
         // if _token != token, there are no restrictions
-        // if _token = token, only unallocated tokens can be transferred, 
+        // if _token == token, only unallocated tokens can be transferred,
+    }
+
+    /* Private functions */
+
+    function isValidGeneration(uint256 _generation)
+        private
+        view
+        returns (bool valid_)
+    {
+        // generation must not be zero (undefined) and
+        // not have expired
+        valid_ = generations[_generation] != uint256(0) &&
+            _generation >= block.timestamp;
     }
 }
